7c95057a027793a1f275e0c025957714763d507c
Zelly Snyder <zelcon@zelcon.net>
2024-02-05 20:49:38 -0800

    Change API base URL

--- web/api/config.ts
+++ web/api/config.ts
@@ -2,6 +2,6 @@ let baseUrl = 'http://localhost:8080';
 if (process.env.NODE_ENV === 'development') {
     baseUrl = 'http://localhost:8080';
 } else {
-    baseUrl = "https://api.wikidice.com";
+    baseUrl = "https://wikidice.zelcon.net/api/";
 }
 export default baseUrl;
